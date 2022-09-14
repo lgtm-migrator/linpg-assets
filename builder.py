8579b1193eff3c285090979cf4c5c6aa7cb37f16
AnyLeftovers <37920527+yudonglin@users.noreply.github.com>
2022-09-14 12:14:50 -0700

    change the way that tiles are stored and managed

--- builder.py
+++ builder.py
@@ -5,5 +5,5 @@ additional_files: tuple[str, ...] = ("README.md", "LICENSE")
 Builder.compile("linpgassets", additional_files=additional_files)
 
 # 打包上传最新的文件
-if input("Do you want to package and upload the lastest build (Y/n):") == "Y":
+if input("Do you want to package and upload the latest build (Y/n):") == "Y":
     Builder.upload_package()
